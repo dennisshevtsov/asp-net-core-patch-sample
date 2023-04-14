e2c06e4995cf3150e25b83b530cb9a0e2d4949ff
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-14 20:25:34 +0300

    add order repository

---
+++ src/AspNetPatchSample.Domain/Repository/IOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Domain.Repository
+{
+  using AspNetPatchSample.Domain.Entity;
+
+  /// <summary>Provides a simple API to store instances of the <see cref="AspNetPatchSample.Domain.Entity.IOrderEntity"/>.</summary>
+  public interface IOrderRepository
+  {
+    /// <summary>Gets an order.</summary>
+    /// <param name="orderIdentity">An object that represents an order identity.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="AspNetPatchSample.Domain.Entity.IOrderEntity"/>.</returns>
+    public Task<IOrderEntity> GetOrderAsync(IOrderIdentity orderIdentity, CancellationToken cancellationToken);
+
+    /// <summary>Updates an order.</summary>
+    /// <param name="orderEntity"></param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task UpdateAsync(IOrderEntity orderEntity, CancellationToken cancellationToken);
+  }
+}
