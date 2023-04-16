3b37781646dca2c54b9cb273dea1b4b8508f6b02
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-17 01:37:19 +0300

    add dtos for get book request

---
+++ src/AspNetPatchSample.WebApi/Dtos/GetBookRequestDto.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.WebApi.Dtos
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  using AspNetPatchSample.Domain.Entity;
+
+  /// <summary>Represents a condition to query a book.</summary>
+  public sealed class GetBookRequestDto : IBookIdentity
+  {
+    /// <summary>Gets an object that represents an ID of a book.</summary>
+    [FromRoute]
+    public Guid BookId { get; }
+  }
+}
