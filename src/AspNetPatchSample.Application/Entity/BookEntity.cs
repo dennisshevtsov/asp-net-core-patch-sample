820e37217531f63b74c9252e5adc626d846f0c64
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-18 23:58:01 +0300

    add update methods to book entity

--- src/AspNetPatchSample.Application/Entity/BookEntity.cs
+++ src/AspNetPatchSample.Application/Entity/BookEntity.cs
@@ -22,15 +22,43 @@ namespace AspNetPatchSample.Application.Entity
     public Guid BookId { get; }
 
     /// <summary>Gets an object that represents a name of a book.</summary>
-    public string Name { get; }
+    public string Name { get; private set; }
 
     /// <summary>Gets an object that represents a description of a book.</summary>
-    public string Author { get; }
+    public string Author { get; private set; }
 
     /// <summary>Gets an object that represents a description of a book.</summary>
-    public string Description { get; }
+    public string Description { get; private set; }
 
     /// <summary>Gets an object that represents a description of a book.</summary>
-    public int Pages { get; }
+    public int Pages { get; private set; }
+
+    /// <summary>Updates this book.</summary>
+    /// <param name="bookData">An object that represents book data.</param>
+    public void Update(IBookData bookData)
+    {
+      Name        = bookData.Name;
+      Author      = bookData.Author;
+      Description = bookData.Description;
+      Pages       = bookData.Pages;
+    }
+
+    /// <summary>Updates this book.</summary>
+    /// <param name="bookData">An object that represents book data.</param>
+    /// <param name="properties">An object that represents a collection of properties to update.</param>
+    public void Update(IBookData bookData, string[] properties)
+    {
+      for (int i = 0; i < properties.Length; ++i)
+      {
+        var property = typeof(IBookData).GetProperty(properties[i]);
+
+        if (property != null)
+        {
+          var value = property.GetValue(bookData);
+
+          property.SetValue(this, value);
+        }
+      }
+    }
   }
 }
