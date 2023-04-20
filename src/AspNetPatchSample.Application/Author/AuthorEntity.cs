f62ea245f7a1c0df3c04ea511805c3437789ea03
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-21 00:11:42 +0300

    add author entity

--- src/AspNetPatchSample.Application/Author/AuthorEntity.cs
+++ src/AspNetPatchSample.Application/Author/AuthorEntity.cs
@@ -2,27 +2,27 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace AspNetPatchSample.Infrastructure.Author
+namespace AspNetPatchSample.Application.Author
 {
   using AspNetPatchSample.Domain.Author;
 
   /// <summary>Represents an author entity.</summary>
   public sealed class AuthorEntity : IAuthorEntity
   {
-    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Infrastructure.Author.AuthorEntity"/> class.</summary>
+    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Application.Author.AuthorEntity"/> class.</summary>
     public AuthorEntity()
     {
       Name = string.Empty;
     }
 
-    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Infrastructure.Author.AuthorEntity"/> class.</summary>
+    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Application.Author.AuthorEntity"/> class.</summary>
     /// <param name="authorData">An object that represents author data.</param>
     public AuthorEntity(IAuthorData authorData)
     {
       Name = authorData.Name;
     }
 
-    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Infrastructure.Author.AuthorEntity"/> class.</summary>
+    /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Application.Author.AuthorEntity"/> class.</summary>
     /// <param name="authorEntity">An object that represents an author entity.</param>
     public AuthorEntity(IAuthorEntity authorEntity)
     {
