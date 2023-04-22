c0eba039eb9eca6e19977b56c3f4f762fbf98b06
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-22 19:40:13 +0300

    add put author action

--- src/AspNetPatchSample.Web/Author/AuthorController.cs
+++ src/AspNetPatchSample.Web/Author/AuthorController.cs
@@ -48,5 +48,17 @@ namespace AspNetPatchSample.Web.Author
     {
       return Task.FromResult<IActionResult>(NoContent());
     }
+
+    /// <summary>Handles the PUT author request.</summary>
+    /// <param name="requestDto">An object that represents the PUT author request data.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="Microsoft.AspNetCore.Mvc.IActionResult"/>.</returns>
+    [HttpPut(Name = nameof(AuthorController.PutAuthor))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [Consumes(typeof(PutAuthorRequestDto), "application/json")]
+    public Task<IActionResult> PutAuthor(PutAuthorRequestDto requestDto, CancellationToken cancellationToken)
+    {
+      return Task.FromResult<IActionResult>(NoContent());
+    }
   }
 }
