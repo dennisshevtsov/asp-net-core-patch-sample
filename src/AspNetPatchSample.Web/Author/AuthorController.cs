dec050ce0fa59d7add118d7a95e96ec21ab3461b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-22 19:11:13 +0300

    add author controller

---
+++ src/AspNetPatchSample.Web/Author/AuthorController.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Web.Author
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  /// <summary>Provides a simple API to handle HTTP request.</summary>
+  [ApiController]
+  [Route("api/author")]
+  [Produces("application/json")]
+  public sealed class AuthorController
+  {
+  }
+}
