fddd5ca335b51e365ea99dae566438aeeb0fdec7
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-24 00:11:47 +0300

    add base entity

---
+++ src/AspNetPatchSample.Data/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Data
+{
+  /// <summary>Represents a base entity.</summary>
+  public abstract class EntityBase
+  {
+    /// <summary>Gets/sets an object that represents an ID of an entity.</summary>
+    public Guid Id { get; set; }
+  }
+}
