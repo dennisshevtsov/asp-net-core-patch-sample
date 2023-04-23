087ab6d03fb76029ce5cd3727c7fd9e956342751
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-24 00:25:43 +0300

    remove get author

--- src/AspNetPatchSample.Data/Author/AuthorRepository.cs
+++ src/AspNetPatchSample.Data/Author/AuthorRepository.cs
@@ -15,15 +15,5 @@ namespace AspNetPatchSample.Author.Data
     /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Author.Data.AuthorRepository"/> class.</summary>
     /// <param name="dbContext">An object that represents a session with the database and can be used to query and save instances of your entities.</param>
     public AuthorRepository(DbContext dbContext) : base(dbContext) { }
-
-    /// <summary>Gets an author by an ID of this entity.</summary>
-    /// <param name="identity">An object that represents an author identity.</param>
-    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
-    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="IAuthorEntity"/>.</returns>
-    public async Task<IAuthorEntity?> GetAsync(IAuthorIdentity identity, CancellationToken cancellationToken)
-      => await DbContext.Set<AuthorEntity>()
-                        .AsNoTracking()
-                        .Where(entity => entity.AuthorId == identity.AuthorId)
-                        .FirstOrDefaultAsync(cancellationToken);
   }
 }
