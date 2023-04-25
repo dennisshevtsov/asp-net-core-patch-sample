2a0cf71140a94a2d4cee46c18ab6f671ebad17ba
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-25 12:37:15 +0300

    add updatable interface

---
+++ src/AspNetPatchSample.App/IUpdatable.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.App
+{
+  /// <summary>Represents contract to update an entity.</summary>
+  public interface IUpdatable<TEntity>
+  {
+    /// <summary>Updates this entity.</summary>
+    /// <param name="newEntity">An object that represents an entity from which this entity should be updated.</param>
+    public void Update(TEntity newEntity);
+
+    /// <summary>Updates this entity.</summary>
+    /// <param name="newEntity">An object that represents an entity from which this entity should be updated.</param>
+    /// <param name="properties">An object that represents a collection of properties to update.</param>
+    public void Update(TEntity newEntity, string[] properties);
+  }
+}
