3453f18a033a01838dcfa0733cc038795c111d28
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-28 00:04:08 +0300

    add request dto binder provider

---
+++ src/AspNetPatchSample.Web/Binding/RequestDtoBinderProvider.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Web.Binding
+{
+  using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+  /// <summary>Provides a simple API to create an instance of the <see cref="AspNetPatchSample.Web.Binding.RequestDtoBinder"/> class.</summary>
+  public sealed class RequestDtoBinderProvider : IModelBinderProvider
+  {
+    /// <summary>Creates a <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.IModelBinder"/> based on <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.ModelBinderProviderContext"/>.</summary>
+    /// <param name="context">A context object for <see cref="IModelBinderProvider.GetBinder"/>.</param>
+    /// <returns>An instance of the <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.IModelBinder"/>.</returns>
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+      if (context.Metadata.ModelType.IsAssignableTo(typeof(RequestDtoBase)))
+      {
+        return new RequestDtoBinder();
+      }
+
+      return null;
+    }
+  }
+}
