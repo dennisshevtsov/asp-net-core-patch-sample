3ce2dd799a2d081ff85b479e3308ce4d449f0d0e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-28 00:03:49 +0300

    add request dto binder

---
+++ src/AspNetPatchSample.Web/Binding/RequestDtoBinder.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Web.Binding
+{
+  using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+  /// <summary>Provides a simple API to create an instance of a model from HTTP request.</summary>
+  public sealed class RequestDtoBinder : IModelBinder
+  {
+    /// <summary>Attempts to bind a model.</summary>
+    /// <param name="bindingContext">A object that contains operating information for model binding and validation.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+      return Task.CompletedTask;
+    }
+  }
+}
