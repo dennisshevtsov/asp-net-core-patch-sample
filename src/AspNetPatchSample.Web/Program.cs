37370b574817065f45a8821dc9e570293f2e6b5f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-28 00:04:28 +0300

    register binder

--- src/AspNetPatchSample.Web/Program.cs
+++ src/AspNetPatchSample.Web/Program.cs
@@ -2,10 +2,12 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using AspNetPatchSample.Web.Binding;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.ModelBinderProviders.Insert(0, new RequestDtoBinderProvider()));
 
 builder.Services.SetUpApplication();
 builder.Services.SetUpInfrastructure();
