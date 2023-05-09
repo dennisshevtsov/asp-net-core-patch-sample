5dbcb9a721bfc3d96313fb8f0f5841a3590c9de8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-09 22:06:27 +0300

    include author data property

--- src/AspNetPatchSample.Data/Book/BookRepository.cs
+++ src/AspNetPatchSample.Data/Book/BookRepository.cs
@@ -12,8 +12,6 @@ namespace AspNetPatchSample.Book.Data
   using AspNetPatchSample;
   using AspNetPatchSample.Book;
   using AspNetPatchSample.Data;
-  using AspNetPatchSample.Data.Book;
-  using System.Collections.Generic;
 
   /// <summary>Provides a simple API to store instances of the <see cref="AspNetPatchSample.Book.IBookEntity"/>.</summary>
   public sealed class BookRepository : RepositoryBase<IBookEntity, BookEntity>, IBookRepository
@@ -25,41 +23,8 @@ namespace AspNetPatchSample.Book.Data
     public override async Task<IBookEntity?> GetAsync(IIdentity identity, CancellationToken cancellationToken)
       => await DbContext.Set<BookEntity>()
                         .AsNoTracking()
-                        .Include(entity => entity.Authors)
+                        .Include(entity => entity.BookAuthors)
                         .Where(entity => entity.Id == identity.Id)
                         .SingleOrDefaultAsync(cancellationToken);
-
-    public override async Task<IBookEntity> AddAsync(IBookEntity entity, CancellationToken cancellationToken)
-    {
-      var dbBookEntity = await base.AddAsync(entity, cancellationToken);
-      await AddBookAuthors(entity, cancellationToken);
-
-      return dbBookEntity;
-    }
-
-    public override async Task UpdateAsync(IBookEntity bookEntity, IEnumerable<string> properties, CancellationToken cancellationToken)
-    {
-      if (properties.Contains(nameof(IBookEntity.Authors)))
-      {
-        await DbContext.Set<BookAuthorEntity>()
-                       .Where(entity => entity.BookId == bookEntity.Id)
-                       .ExecuteDeleteAsync(cancellationToken);
-
-        await AddBookAuthors(bookEntity, cancellationToken);
-      }
-
-      await base.UpdateAsync(bookEntity, properties, cancellationToken);
-    }
-
-    private Task AddBookAuthors(IBookEntity bookEntity, CancellationToken cancellationToken)
-    {
-      var bookAuthorEntities =
-        bookEntity.Authors.Select(entity => new BookAuthorEntity(bookEntity.Id, entity.Id))
-                          .ToArray();
-
-      DbContext.AddRange(bookAuthorEntities);
-
-      return DbContext.SaveChangesAsync(cancellationToken);
-    }
   }
 }
