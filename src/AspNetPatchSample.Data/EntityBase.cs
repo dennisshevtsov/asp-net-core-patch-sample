fde4e06909bcfadfcafe425d58c4e33be12b4f45
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-10 23:13:46 +0300

    override equals in entity base

--- src/AspNetPatchSample.Data/EntityBase.cs
+++ src/AspNetPatchSample.Data/EntityBase.cs
@@ -9,5 +9,22 @@ namespace AspNetPatchSample.Data
   {
     /// <summary>Gets/sets an object that represents an ID of an entity.</summary>
     public Guid Id { get; set; }
+
+    /// <summary>Determines whether the specified object is equal to the current object.</summary>
+    /// <param name="obj">The object to compare with the current object</param>
+    /// <returns>An object that indicates if it equals to an comparing object.</returns>
+    public override bool Equals(object? obj)
+    {
+      if (obj is IIdentity identity)
+      {
+        return identity.Id == Id;
+      }
+
+      return false;
+    }
+
+    /// <summary>Serves as the default hash function.</summary>
+    /// <returns>A hash code for the current object.</returns>
+    public override int GetHashCode() => Id.GetHashCode();
   }
 }
