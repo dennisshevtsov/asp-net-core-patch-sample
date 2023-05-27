e85493d70dc6890bd87f0415d85147c5ed24606c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-27 21:41:53 +0300

    update app

--- src/AspNetPatchSample.App/Book/BookEntity.cs
+++ src/AspNetPatchSample.App/Book/BookEntity.cs
@@ -4,21 +4,24 @@
 
 namespace AspNetPatchSample.Book.App
 {
-  using AspNetPatchSample.App;
   using AspNetPatchSample.Author;
 
   /// <summary>Represents a book entity.</summary>
-  public sealed class BookEntity : EntityBase, IBookEntity, IUpdatable<IBookEntity>
+  public sealed class BookEntity : IBookEntity
   {
     /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.Book.App.BookEntity"/> class.</summary>
     /// <param name="bookEntity">An object that represents a book entity.</param>
-    public BookEntity(IBookEntity bookEntity) : base(bookEntity)
+    public BookEntity(IBookEntity bookEntity)
     {
+      BookId      = bookEntity.BookId;
       Title       = bookEntity.Title;
       Description = bookEntity.Description;
       Pages       = bookEntity.Pages;
       Authors     = bookEntity.Authors;
     }
+
+    /// <summary>Gets an object that represents an ID of a book.</summary>
+    public Guid BookId { get; }
 
     /// <summary>Gets/sets an object that represents a title of a book.</summary>
     public string Title { get; private set; }
@@ -31,17 +34,5 @@ namespace AspNetPatchSample.Book.App
 
     /// <summary>Gets an object that represents a collection of authors of this book.</summary>
     public IEnumerable<IAuthorEntity> Authors { get; private set; }
-
-    /// <summary>Updates this book.</summary>
-    /// <param name="bookEntity">An object that represents a book entity from which this book should be updated..</param>
-    /// <returns>A reference to this book.</returns>
-    public IBookEntity Update(IBookEntity bookEntity) => (IBookEntity)base.Update(bookEntity);
-
-    /// <summary>Updates this book.</summary>
-    /// <param name="bookEntity">An object that represents a book entity from which this book should be updated..</param>
-    /// <param name="properties">An object that represents a collection of properties to update.</param>
-    /// <returns>A reference to this book.</returns>
-    public IBookEntity Update(IBookEntity bookEntity, IEnumerable<string> properties) =>
-      (IBookEntity)base.Update(bookEntity, properties);
   }
 }
--- src/AspNetPatchSample.App/ServiceBase.cs
+++ src/AspNetPatchSample.App/ServiceBase.cs
@@ -5,20 +5,15 @@
 namespace AspNetPatchSample.App
 {
   /// <summary>Provides a simple GRUD API.</summary>
+  /// <typeparam name="TEntity">An entity type.</typeparam>
   /// <typeparam name="TIdentity">An identity type.</typeparam>
-  /// <typeparam name="TDomainEntity">A domain entity type.</typeparam>
-  /// <typeparam name="TBusinessEntity">A buisiness entity type.</typeparam>
-  public abstract class ServiceBase<TIdentity, TDomainEntity, TBusinessEntity>
-    : IService<TIdentity, TDomainEntity>
-    where TIdentity : IIdentity
-    where TDomainEntity : class
-    where TBusinessEntity : EntityBase, TDomainEntity, IUpdatable<TDomainEntity>
+  public abstract class ServiceBase<TEntity, TIdentity> : IService<TEntity, TIdentity>
   {
-    private readonly IRepository<TDomainEntity> _repository;
+    private readonly IRepository<TEntity, TIdentity> _repository;
 
     /// <summary>Initializes a new instance of the <see cref="AspNetPatchSample.App.ServiceBase{TEntity, TData, TEntity}"/> class.</summary>
     /// <param name="repository">An object that provides a simple API to store instances of the <see cref="TEntity"/>.</param>
-    protected ServiceBase(IRepository<TDomainEntity> repository)
+    protected ServiceBase(IRepository<TEntity, TIdentity> repository)
     {
       _repository = repository ?? throw new ArgumentNullException(nameof(repository));
     }
@@ -27,27 +22,24 @@ namespace AspNetPatchSample.App
     /// <param name="identity">An object that represents an identity to get.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="AspNetPatchSample.Author.IAuthorEntity"/>. The result can be null.</returns>
-    public virtual Task<TDomainEntity?> GetAsync(TIdentity identity, CancellationToken cancellationToken)
+    public virtual Task<TEntity?> GetAsync(TIdentity identity, CancellationToken cancellationToken)
       => _repository.GetAsync(identity, cancellationToken);
 
     /// <summary>Adds an entity.</summary>
-    /// <param name="data">An object that represents data from that a new entity should be created.</param>
+    /// <param name="entity">An object that represents data from that a new entity should be created.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="AspNetPatchSample.Author.IAuthorEntity"/>.</returns>
-    public virtual Task<TDomainEntity> AddAsync(TDomainEntity data, CancellationToken cancellationToken)
-      => _repository.AddAsync((TBusinessEntity)Activator.CreateInstance(typeof(TBusinessEntity), data)!, cancellationToken);
+    public virtual Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken)
+      => _repository.AddAsync(entity, cancellationToken);
 
     /// <summary>Updates an entity.</summary>
     /// <param name="originalEntity">An object that represents an entity to update.</param>
     /// <param name="newEntity">An object that represents an entity from that the original one should be updated.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that represents an asynchronous operation.</returns>
-    public virtual Task UpdateAsync(TDomainEntity originalEntity, TDomainEntity newEntity, CancellationToken cancellationToken)
+    public virtual Task UpdateAsync(TEntity originalEntity, TEntity newEntity, CancellationToken cancellationToken)
     {
-      var buisinessEntity = (TBusinessEntity)Activator.CreateInstance(typeof(TBusinessEntity), originalEntity)!;
-      buisinessEntity.Update(newEntity);
-
-      return _repository.UpdateAsync(buisinessEntity, buisinessEntity.Properties, cancellationToken);
+      throw new NotImplementedException();
     }
 
     /// <summary>Updates an entity partially.</summary>
@@ -56,12 +48,9 @@ namespace AspNetPatchSample.App
     /// <param name="properties">An object that represents a collection of properties to update.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that represents an asynchronous operation.</returns>
-    public virtual Task UpdateAsync(TDomainEntity originalEntity, TDomainEntity newEntity, IEnumerable<string> properties, CancellationToken cancellationToken)
+    public virtual Task UpdateAsync(TEntity originalEntity, TEntity newEntity, IEnumerable<string> properties, CancellationToken cancellationToken)
     {
-      var buisinessEntity = (TBusinessEntity)Activator.CreateInstance(typeof(TBusinessEntity), originalEntity)!;
-      buisinessEntity.Update(newEntity, properties);
-
-      return _repository.UpdateAsync(buisinessEntity, properties, cancellationToken);
+      throw new NotImplementedException();
     }
 
     /// <summary>Deletes an entity.</summary>
