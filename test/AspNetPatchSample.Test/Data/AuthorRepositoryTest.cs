b87dbc2df1b58bc021af1f62ac5eccd69a168ecf
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-29 00:33:55 +0300

    add author repository test

--- test/AspNetPatchSample.Test/Data/AuthorRepositoryTest.cs
+++ test/AspNetPatchSample.Test/Data/AuthorRepositoryTest.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
 
 namespace AspNetPatchSample.Test.Data
 {
+  using Microsoft.Extensions.DependencyInjection;
+
+  using AspNetPatchSample.Author;
+
   [TestClass]
-  public sealed class AuthorRepositoryTest
+  public sealed class AuthorRepositoryTest : DataTestBase
   {
+#pragma warning disable CS8618
+    private IAuthorRepository _authorRepository;
+#pragma warning restore CS8618
+
+    protected override void Initialize(IServiceProvider provider)
+    {
+      _authorRepository = provider.GetRequiredService<IAuthorRepository>();
+    }
   }
 }
