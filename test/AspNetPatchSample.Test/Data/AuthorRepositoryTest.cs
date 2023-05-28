e32b78032e29fe7abebc16619227bccfbc724691
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-29 00:32:13 +0300

    add data test base

---
+++ test/AspNetPatchSample.Test/Data/AuthorRepositoryTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetPatchSample.Test.Data
+{
+  [TestClass]
+  public sealed class AuthorRepositoryTest
+  {
+  }
+}
