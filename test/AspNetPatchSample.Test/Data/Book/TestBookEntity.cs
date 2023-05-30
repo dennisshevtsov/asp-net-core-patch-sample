95df22c2f1dcde6ace22cac0c3f10a280b6da428
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-30 23:10:25 +0300

    add test book

---
+++ test/AspNetPatchSample.Test/Data/Book/TestBookEntity.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using AspNetPatchSample.Author;
+
+namespace AspNetPatchSample.Book.Data.Test
+{
+  public sealed class TestBookEntity : IBookEntity
+  {
+    public Guid BookId { get; set; }
+
+    public string Title { get; }
+
+    public string Description { get; }
+
+    public int Pages { get; }
+
+    public IEnumerable<IAuthorEntity> Authors { get; }
+  }
+}
