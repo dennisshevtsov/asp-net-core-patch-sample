ca3ef84d804bd19e1e68670f3f5fc4dfcfb9aa13
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-30 23:11:57 +0300

    add book indentity

---
+++ test/AspNetPatchSample.Test/Data/Book/TestBookIdentity.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Book.Data.Test
+{
+  public sealed class TestBookIdentity : IBookIdentity
+  {
+    public TestBookIdentity(Guid bookdId)
+    {
+      BookId = bookdId;
+    }
+
+    public Guid BookId { get; }
+
+    public static IBookIdentity New() => new TestBookIdentity(Guid.NewGuid());
+  }
+}
