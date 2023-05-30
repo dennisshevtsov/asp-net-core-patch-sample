d3efc6e2e915d121ae6347f27d69566cd19952b5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-30 23:09:03 +0300

    add book integration test

---
+++ test/AspNetPatchSample.Test/Data/Book/BookRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.Book.Data.Test
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  using AspNetPatchSampl.Data.Test;
+
+  [TestClass]
+  public sealed class BookRepositoryTest : DataTestBase
+  {
+#pragma warning disable CS8618
+    private IBookRepository _bookRepository;
+#pragma warning restore CS8618
+
+    protected override void Initialize(IServiceProvider provider)
+    {
+      _bookRepository = provider.GetRequiredService<IBookRepository>();
+    }
+  }
+}
