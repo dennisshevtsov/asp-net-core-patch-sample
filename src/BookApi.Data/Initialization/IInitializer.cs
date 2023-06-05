279417f9f1e76b56f891cbc768874d5ec1c77cc7
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-06 02:23:26 +0300

    add initializer interface

---
+++ src/BookApi.Data/Initialization/IInitializer.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace BookApi.Data.Initialization
+{
+  /// <summary>Provides a simple API to set up the database.</summary>
+  public interface IInitializer
+  {
+    public Task SetUpAsync(CancellationToken cancellationToken);
+  }
+}
