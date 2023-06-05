b38138f31d2f786b23bf98619ddbeac5e6430fa4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-05 22:19:25 +0300

    add fabric method

--- src/AspNetPatchSample.App/EntityBase.cs
+++ src/AspNetPatchSample.App/EntityBase.cs
@@ -17,5 +17,22 @@ namespace AspNetPatchSample.App
 
     /// <summary>Gets/sets an object that represents a collection of updated properties.</summary>
     public IEnumerable<string> Properties => _properties;
+
+    /// <summary>Creates a copy of an entity.</summary>
+    /// <param name="entity">An object that represents an entity to copy.</param>
+    /// <returns>An object that represents an instance of an entity copy.</returns>
+    /// <exception cref="System.NotSupportedException">Throws if there is no such entity.</exception>
+    public static T2 Create<T1, T2>(T1 entity) where T2 : EntityBase, T1
+    {
+      ArgumentNullException.ThrowIfNull(entity);
+
+      if (entity.GetType() == typeof(T2))
+      {
+        return (T2)entity;
+      }
+
+      return (T2)typeof(T2).GetConstructor(new[] { typeof(T1) })!
+                           .Invoke(new object[] { entity! });
+    }
   }
 }
