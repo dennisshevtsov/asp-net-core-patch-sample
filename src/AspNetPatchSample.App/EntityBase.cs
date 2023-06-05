7492bd5f0747cc0ea829fab955966c2248badfae
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-05 22:16:20 +0300

    add base entity

---
+++ src/AspNetPatchSample.App/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace AspNetPatchSample.App
+{
+  public abstract class EntityBase
+  {
+    private readonly IList<string> _properties;
+
+    protected EntityBase()
+    {
+      _properties = new List<string>();
+    }
+
+    public IEnumerable<string> Properties => _properties;
+  }
+}
