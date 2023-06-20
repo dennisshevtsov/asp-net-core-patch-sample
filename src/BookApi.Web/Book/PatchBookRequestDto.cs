124e8302db4b25c748c6241442b6e85962e2a515
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-20 22:26:12 +0300

    move author dto to root

--- src/BookApi.Web/Book/PatchBookRequestDto.cs
+++ src/BookApi.Web/Book/PatchBookRequestDto.cs
@@ -49,21 +49,5 @@ namespace BookApi.Book.Web
     /// <summary>Gets an object that represents a collection of properties to update.</summary>
     [JsonIgnore]
     public IEnumerable<string> Properties { get; set; }
-
-    /// <summary>Represents an author entity.</summary>
-    public sealed class AuthorDto : IAuthorEntity
-    {
-      /// <summary>Initializes a new instance of the <see cref="BookApi.Book.Web.GetBookResponseDto.AuthorDto"/> class.</summary>
-      public AuthorDto()
-      {
-        Name = string.Empty;
-      }
-
-      /// <summary>Gets an object that represents an ID of an author.</summary>
-      public Guid AuthorId { get; set; }
-
-      /// <summary>Gets an object that represents a name of an author.</summary>
-      public string Name { get; set; }
-    }
   }
 }
