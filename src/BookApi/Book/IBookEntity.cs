6c0bc34e0bdb9c4122352ee867413aafad3ec8a6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-31 00:23:25 +0300

    move usings from namespace

--- src/BookApi/Book/IBookEntity.cs
+++ src/BookApi/Book/IBookEntity.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace BookApi.Book;
-
 using BookApi.Author;
+
+namespace BookApi.Book;
 
 /// <summary>Represents a book entity.</summary>
 public interface IBookEntity : IBookIdentity
